c50225bdd79c5532eb59325d0a31beab6274a54a
Patricia <patriciasilva.mural@gmail.com>
2020-04-04 00:53:21 +0100

    Making the code public
    I forgot to make my project public, sorry

--- src/Singleton/connectBD.java
+++ src/Singleton/connectBD.java
@@ -13,6 +13,7 @@ public class connectBD {
 		String driver = "com.mysql.cj.jdbc.Driver";
 		String user = "cctstudent";
 		String password = "Pass1234!";
+		
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		    conn = DriverManager.getConnection("jdbc:mysql://52.50.23.197:3306/world", "cctstudent", "Pass1234!");
