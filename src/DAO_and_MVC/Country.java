06abcff78e54713dab0e190eba8b628ce41c6596
Patricia <patriciasilva.mural@gmail.com>
2020-04-04 01:23:16 +0100

    Country class modified
    Adding encapsulation

--- src/DAO_and_MVC/Country.java
+++ src/DAO_and_MVC/Country.java
@@ -1,59 +1,72 @@
 package DAO_and_MVC;
 
-// this class its a model, country 
+//This class its a model, country 
 public class Country {
 	
-	private String code ;
-	private String name ;
-	private continent continent;
-    private float surfacearea;
-    private String headofstate ;
+	private String code ; //Making the variable private for encapsulation
+	private String name ; //Making the variable private for encapsulation
+	private continent continent; //Making the variable private for encapsulation
+    private float surfacearea; //Making the variable private for encapsulation
+    private String headofstate ; //Making the variable private for encapsulation
     
+    //Constructor overloading setting parameter list
 	public Country(String code, String name, float surfacearea, String headofstate, continent c) {
 		super();
+		
+		//Initializing class fields for code, name, surfacearea, headofstate and continent
 		this.code = code;
 		this.name = name;
 		this.surfacearea = surfacearea;
 		this.headofstate = headofstate;
 		this.continent = c;
 	}
-
+	
+	//Use of getters and setters to access all class fields
 	public String getCode() {
 		return code;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public void setCode(String code) {
 		this.code = code;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public String getName() {
 		return name;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public float getSurfacearea() {
 		return surfacearea;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public void setSurfacearea(float surfacearea) {
 		this.surfacearea = surfacearea;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public String getHeadofstate() {
 		return headofstate;
 	}
 	
+	//Use of getters and setters to access all class fields
 	public void setHeadofstate(String headofstate) {
 		this.headofstate = headofstate;
 	}
 
+	//Use of getters and setters to access all class fields
 	public continent getContinent() {
 		return continent;
 	}
 
+	//Use of getters and setters to access all class fields
 	public void setContinent(continent continent) {
 		this.continent = continent;
 	}
