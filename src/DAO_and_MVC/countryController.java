6902f465751263197a431030a722f9824cd32f1a
Patricia <patriciasilva.mural@gmail.com>
2020-04-05 00:39:27 +0100

    Countrycontroller updates
    Countrycontroller.java use clientProgram.java to create and list

--- src/DAO_and_MVC/countryController.java
+++ src/DAO_and_MVC/countryController.java
@@ -1,37 +1,40 @@
 package DAO_and_MVC;
 
+//Importing java.util library for the use of ArrayList
 import java.util.ArrayList;
 import java.util.List;
 
 //Countrycontroller.java use clientProgram.java to create and list
-public class countryController {
+//countryControler is used here to deal with requests for the country entity
+
+public class countryController { //public class countryController
 	
-	private viewCountry vista= new viewCountry();
+	private viewCountry vista= new viewCountry(); //Creating an object for the class called vista which will store the countries
 	
-	public countryController() {
+	public countryController() { //Default constructor
 	}
 	
 	public void register(Country country) {
-		IcountryDAO dao= new  countryDAOimplement();
-		dao.create(country);
+		IcountryDAO dao= new  countryDAOimplement(); //Creating an object to provides access to the database or any other storage
+		dao.create(country); //Creating the object itself
 	}
 
-	public void list_countrys(){
-		List<Country> countrys = new ArrayList<Country>();
-		IcountryDAO dao= new  countryDAOimplement();
-		countrys=dao.list();
-		vista.list_countrys(countrys);
+	public void list_countrys(){ //list_country method
+		List<Country> countrys = new ArrayList<Country>(); //Array List is used here to store the list of countries
+		IcountryDAO dao= new  countryDAOimplement(); //Implementation of the DAO object to store the country
+		countrys=dao.list(); //Countries will be presented in the DAO list
+		vista.list_countrys(countrys); //method return the list of countries
 	}
 	
-	public void list_countrys_by_code(String code){
-		IcountryDAO dao = new  countryDAOimplement();
-		Country country = dao.list_by_code(code);
-		vista.list_countrys_by_code(country);
+	public void list_countrys_by_code(String code){ //list_country_by_code method that passes a String code as parameter
+		IcountryDAO dao = new  countryDAOimplement(); //Implementation of the DAO object to store the country
+		Country country = dao.list_by_code(code); //Implementation of the DAO object to store the list of countries by code
+		vista.list_countrys_by_code(country); //method return the list of countries by code
 	}
 	
-	public void list_countrys_by_name(String name){
-		IcountryDAO dao = new  countryDAOimplement();
-		Country country = dao.list_by_name(name);
-		vista.list_countrys_by_name(country);
+	public void list_countrys_by_name(String name){ //list_country_by_name method that passes a String name as parameter
+		IcountryDAO dao = new  countryDAOimplement(); //Implementation of the DAO object to store the country
+		Country country = dao.list_by_name(name); //Implementation of the DAO object to store the list of countries by name
+		vista.list_countrys_by_name(country); //method return the list of countries by name
 	}
 }
