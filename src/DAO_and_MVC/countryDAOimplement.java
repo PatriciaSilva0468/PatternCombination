cde2d28045dc3b8ebfa90ad43f84e7b92221bba2
Patricia <patriciasilva.mural@gmail.com>
2020-04-05 01:08:46 +0100

    CountryDAOimplement class
    Added comments

--- src/DAO_and_MVC/countryDAOimplement.java
+++ src/DAO_and_MVC/countryDAOimplement.java
@@ -14,20 +14,21 @@ public class countryDAOimplement implements IcountryDAO {
 	public boolean create(Country country) {
 		boolean register = false;
 		
-		//Establishing a Connection to the database for insertation of code, name, surfacearea, headofstate and continent
-		Statement stm= null;
-		Connection con = connectBD.getConnection();
+		//Establishing a Connection to the database for insertion of code, name, surfacearea, headofstate and continent
+		Statement stm= null; //Statement has been used to implement SQL statements with null parameters
+		Connection con = connectBD.getConnection(); //Establishing Connection to the database
+		//string sql used to insert data into the database
 		String sql="INSERT INTO country (code,name,surfacearea,headofstate, continent) values ('"+country.getCode()+"','"+country.getName()+"','"+country.getSurfacearea()+"','"+country.getHeadofstate()+"','"+country.getContinent()+"')";
 
 		//Try catch to create the connection and return true for registration then close the application otherwise return "Fail"
 		try{
-			stm= con.createStatement();
-			stm.execute(sql);
-			register=true;
-			stm.close();
-		}catch (SQLException e) {
-			System.out.println("Fail");
-			e.printStackTrace();
+			stm= con.createStatement(); //Creating a statement
+			stm.execute(sql); //To execute a query, the execute method was used
+			register=true; //Returns true if the first object that the query returns is a ResultSet object
+			stm.close(); //Method Statement.close used when we have finished to use a Statement 
+		}catch (SQLException e) { //try catch method for sql exception
+			System.out.println("Fail"); // System will print out fail if there is a problem
+			e.printStackTrace(); /// print stack trace 
 		}
 		
 		//return the record if not previously in the database
@@ -38,9 +39,9 @@ public class countryDAOimplement implements IcountryDAO {
 	
 	//Method to list the countries
 	public List<Country> list() {
-		Connection co =null;
-		Statement stm= null;
-		ResultSet rs=null;
+		Connection co =null; //Established connection
+		Statement stm= null; // Setting statement to null
+		ResultSet rs=null; // Result set will return null
 		
 		//string sql to select the details from the database ordering by code
 		String sql="SELECT * FROM country ORDER BY code";
@@ -52,14 +53,14 @@ public class countryDAOimplement implements IcountryDAO {
 		//Application will connect with the database and create statement, after that sql query will be executed
 		
 		try {			
-			co= connectBD.getConnection();
-			stm=co.createStatement();
-			rs=stm.executeQuery(sql);
-			String code, name, headofstate,continent;
-        	float surfacearea ; 
+			co= connectBD.getConnection(); //method to get connection
+			stm=co.createStatement(); //creating statement
+			rs=stm.executeQuery(sql); //Returns one ResultSet object
+			String code, name, headofstate,continent; //Variable of type string
+        	float surfacearea ; //float used to store surfacearea
         	continent continents = null;
         	
-        	//while sql statement is running get code, name, headof state, surfacearea and continent
+        	//while sql statement is running to get code, name, headof state, surfacearea and continent
 			while (rs.next()) {
 				code = rs.getString(1);
 				name = rs.getString(2);
@@ -68,47 +69,47 @@ public class countryDAOimplement implements IcountryDAO {
 				continent = rs.getString(5); 
 				switch(continent){
 	                case "ASIA":
-	                	continents = DAO_and_MVC.continent.ASIA;
+	                	continents = DAO_and_MVC.continent.ASIA; //DAO and MVC implementation
 	                    break;
 	                    
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "EUROPE":
-	                	continents = DAO_and_MVC.continent.EUROPE;
+	                	continents = DAO_and_MVC.continent.EUROPE; //DAO and MVC implementation
 	                    break;
 	                    
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "NORTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.NORTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.NORTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "AFRICA":
-	                	continents = DAO_and_MVC.continent.AFRICA;
+	                	continents = DAO_and_MVC.continent.AFRICA; //DAO and MVC implementation
 	                    break;
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "OCEANIA":
-	                	continents = DAO_and_MVC.continent.OCEANIA;
+	                	continents = DAO_and_MVC.continent.OCEANIA; //DAO and MVC implementation
 	                    break;
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "ANTARCTICA":
-	                	continents = DAO_and_MVC.continent.ANTARCTICA;
+	                	continents = DAO_and_MVC.continent.ANTARCTICA; //DAO and MVC implementation
 	                    break;
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "SOUTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                    //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	            }
-				//Initializing country as setting default constructor values. This new country will be added to the list of country as c
+				//Initializing country and setting default constructor values. This new country will be added to the list of country as c
 				Country c= new Country(code,name,surfacearea,headofstate,continents);
 				listCountry.add(c);
 			}
-			stm.close();
+			stm.close(); //Close statement
 			rs.close();
-		} catch (SQLException e) {
-			System.out.println("Fail");
+		} catch (SQLException e) { //Try catch for sql exception
+			System.out.println("Fail"); //System print out fail if there is a error
 			
 			//Return "Fail" if the application is not successful
-			e.printStackTrace();
+			e.printStackTrace(); // print stack trace method
 		}
 		
 		//Otherwise return the list of countries
@@ -118,9 +119,9 @@ public class countryDAOimplement implements IcountryDAO {
 	@Override
 	//Setting the parameters to create a public class which will search the countries by code
 	public Country list_by_code(String code_to_search){
-		Connection co =null;
-		Statement stm= null;
-		ResultSet rs=null;
+		Connection co =null; //Connection is equal to null
+		Statement stm= null; //Setting statement to null
+		ResultSet rs=null; //Result set equal to null
 		
 		//String SQL to select the details from the database
 		String sql="SELECT * FROM country WHERE code='"+code_to_search+"'";
@@ -129,12 +130,12 @@ public class countryDAOimplement implements IcountryDAO {
 		//The try catch here is being implemented to test a block of code for errors during execution of the code
 		//Application will connect with the database and create statement, after that sql query will be executed
 		try {			
-			co= connectBD.getConnection();
-			stm=co.createStatement();
-			rs=stm.executeQuery(sql);
-			String code, name, headofstate,continent;
-        	float surfacearea ;
-        	continent continents = null;
+			co= connectBD.getConnection(); //method to get connection
+			stm=co.createStatement(); //creating statement
+			rs=stm.executeQuery(sql); //Returns one ResultSet object
+			String code, name, headofstate,continent; //Variable of type string
+        	float surfacearea ; //float used to store surfacearea
+        	continent continents = null; //continents set as null
         	
         	//while sql statement is running get code, name, headof state, surfacearea and continent
 			while (rs.next()) {
@@ -145,31 +146,31 @@ public class countryDAOimplement implements IcountryDAO {
 				continent = rs.getString(5); 
 				switch(continent){
 	                case "ASIA":
-	                	continents = DAO_and_MVC.continent.ASIA;
+	                	continents = DAO_and_MVC.continent.ASIA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "EUROPE":
-	                	continents = DAO_and_MVC.continent.EUROPE;
+	                	continents = DAO_and_MVC.continent.EUROPE; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "NORTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.NORTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.NORTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "AFRICA":
-	                	continents = DAO_and_MVC.continent.AFRICA;
+	                	continents = DAO_and_MVC.continent.AFRICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "OCEANIA":
-	                	continents = DAO_and_MVC.continent.OCEANIA;
+	                	continents = DAO_and_MVC.continent.OCEANIA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "ANTARCTICA":
-	                	continents = DAO_and_MVC.continent.ANTARCTICA;
+	                	continents = DAO_and_MVC.continent.ANTARCTICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "SOUTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	            }
@@ -178,22 +179,22 @@ public class countryDAOimplement implements IcountryDAO {
 				Country c = new Country(code,name,surfacearea,headofstate, continents);
 				return c;
 			}
-			stm.close();
-			rs.close();
+			stm.close(); //Closing statement
+			rs.close(); //Closing result set
 		} catch (SQLException e) {
 			System.out.println("Fail");
 		//Return "Fail" if the application is not successful
-			e.printStackTrace();
+			e.printStackTrace(); // print stack trace method
 		}
-		return null;
+		return null; //return null
 	}
 
 	@Override
 	//Setting the parameters to create a public class which will search the countries by name
 	public Country list_by_name(String name_to_search){
-		Connection co =null;
-		Statement stm= null;
-		ResultSet rs=null;
+		Connection co =null; //Connection equal to null
+		Statement stm= null; //Setting statement to null
+		ResultSet rs=null; // Result set will return null
 		
 		//String SQL to search for the specific country added into the database
 		String sql="SELECT * FROM country WHERE name='"+name_to_search+"'";
@@ -201,11 +202,11 @@ public class countryDAOimplement implements IcountryDAO {
 		//The try catch here is being implemented to test a block of code for errors during execution of the code
 		//Application will connect with the database and create statement, after that sql query will be executed
 		try {			
-			co= connectBD.getConnection();
-			stm=co.createStatement();
-			rs=stm.executeQuery(sql);
-			String code, name, headofstate, continent;
-        	float surfacearea; 
+			co= connectBD.getConnection(); ////method to get connection
+			stm=co.createStatement(); //creating statement
+			rs=stm.executeQuery(sql); //Returns one ResultSet object
+			String code, name, headofstate, continent; //Variable of type string
+        	float surfacearea; //float used to store surfacearea
         	continent continents = null;
         	
         	//while sql statement is running get code, name, headof state, surfacearea and continent
@@ -217,31 +218,31 @@ public class countryDAOimplement implements IcountryDAO {
 				continent = rs.getString(5); 
 				switch(continent){
 	                case "ASIA":
-	                	continents = DAO_and_MVC.continent.ASIA;
+	                	continents = DAO_and_MVC.continent.ASIA; //DAO and MVC implementation
 	                    break;
 	              //break added to allow the application to break the actual flow of the program as it is going to a new continent after     
 	                case "EUROPE":
-	                	continents = DAO_and_MVC.continent.EUROPE;
+	                	continents = DAO_and_MVC.continent.EUROPE; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "NORTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.NORTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.NORTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "AFRICA":
-	                	continents = DAO_and_MVC.continent.AFRICA;
+	                	continents = DAO_and_MVC.continent.AFRICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "OCEANIA":
-	                	continents = DAO_and_MVC.continent.OCEANIA;
+	                	continents = DAO_and_MVC.continent.OCEANIA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "ANTARCTICA":
-	                	continents = DAO_and_MVC.continent.ANTARCTICA;
+	                	continents = DAO_and_MVC.continent.ANTARCTICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	                case "SOUTH_AMERICA":
-	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA;
+	                	continents = DAO_and_MVC.continent.SOUTH_AMERICA; //DAO and MVC implementation
 	                    break;
 	                  //break added to allow the application to break the actual flow of the program as it is going to a new continent after
 	            }
@@ -250,13 +251,13 @@ public class countryDAOimplement implements IcountryDAO {
 				Country c = new Country(code,name,surfacearea,headofstate,continents);
 				return c;
 			}
-			stm.close();
-			rs.close();
+			stm.close(); //Closing statement
+			rs.close(); //Closing result set
 		} catch (SQLException e) {
 			System.out.println("Fail");
 			//Return "Fail" if the application is not successful
-			e.printStackTrace();
+			e.printStackTrace(); // print stack trace method
 		}
-		return null;
+		return null; // return null
 	}
 }
