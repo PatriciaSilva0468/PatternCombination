e00b8a5490a95863e02007893f7136e0c283707c
Patricia <patriciasilva.mural@gmail.com>
2020-04-05 01:42:40 +0100

    ConnectBD class
    Comments added

--- src/Singleton/connectBD.java
+++ src/Singleton/connectBD.java
@@ -1,35 +1,35 @@
 package Singleton;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection; //Importing java.sql connection for connection with the lecturer database
+import java.sql.DriverManager; //JDBC drivers
+import java.sql.SQLException; //Importing java.sql exception to handle errors with the database
 
-public class connectBD {
+public class connectBD { //Public class connectBD
 
-	private static Connection conn = null;
+	private static Connection conn = null; //Private static connection initializer
 
     private connectBD(){
-		String url = "jdbc:mysql://52.50.23.197:3306/world";
-		String driver = "com.mysql.cj.jdbc.Driver";
-		String user = "cctstudent";
-		String password = "Pass1234!";
+		String url = "jdbc:mysql://52.50.23.197:3306/world"; //Defining path to the database
+		String driver = "com.mysql.cj.jdbc.Driver"; //JDBC Driver path
+		String user = "cctstudent"; //Username of the database in Workbench
+		String password = "Pass1234!"; //Password of the database in Workbench
 		
 		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		    conn = DriverManager.getConnection("jdbc:mysql://52.50.23.197:3306/world", "cctstudent", "Pass1234!");
-		}catch(ClassNotFoundException | SQLException e){
-		    e.printStackTrace();
+			Class.forName("com.mysql.cj.jdbc.Driver"); //Returns the class object
+		    conn = DriverManager.getConnection("jdbc:mysql://52.50.23.197:3306/world", "cctstudent", "Pass1234!"); //Establishing connection with the database
+		}catch(ClassNotFoundException | SQLException e){ //Try catch method to handle exceptions
+		    e.printStackTrace(); //print stack trace method
 		}
     } 
 
-    public static Connection getConnection(){
+    public static Connection getConnection(){ //Method to getConnection
   
-    	if (conn == null){
-    		new connectBD();
-    		System.out.println("success");
+    	if (conn == null){ //If connection is equal to null create new connection with the database
+    		new connectBD(); //If connection is equal to null create new connection with the database
+    		System.out.println("success"); //if application is connect with the database print success
 		}
   
-		return conn;
+		return conn; //otherwise return connection
     }
 }
     
