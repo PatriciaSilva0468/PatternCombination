8de917af8ce4914f5d6da44796c4819d83fa9c22
Patricia <patriciasilva.mural@gmail.com>
2020-04-05 01:14:20 +0100

    IcountryDAO
    Interface for countryDAOimplement to use and create the methods there

--- src/DAO_and_MVC/IcountryDAO.java
+++ src/DAO_and_MVC/IcountryDAO.java
@@ -1,10 +1,10 @@
 package DAO_and_MVC;
-import java.util.List;
+import java.util.List; //importing java.util library
 
 //Interface for countryDAOimplement to use and create the methods there
 public interface IcountryDAO {
-	public boolean create(Country country);
-	public List<Country> list();
-	public Country list_by_code(String code);
-	public Country list_by_name(String name);
+	public boolean create(Country country); //Boolean method to create country
+	public List<Country> list(); //Method to get a list of countries
+	public Country list_by_code(String code); //Method to get a list of countries by code. Use variable of type String
+	public Country list_by_name(String name); //Method to get a list of countries by name. Use variable of type String
 }
