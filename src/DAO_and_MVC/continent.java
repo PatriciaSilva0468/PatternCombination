70f90ef0c52b4fb71a16d8bc717a1916f561c34c
Patricia <patriciasilva.mural@gmail.com>
2020-04-05 15:37:15 +0100

    Continent java class
    About the Enum implementation

--- src/DAO_and_MVC/continent.java
+++ src/DAO_and_MVC/continent.java
@@ -3,7 +3,7 @@
 package DAO_and_MVC;
 
 //Public enum has been implemented to represent the 7 continents
-//Enum is used here just because it is a predefined list of values as in this case the continents will not change
+//Enum is used here to guarantee that the value of the continent matches with the options of the database
 public enum continent {
 	ASIA,
 	EUROPE,
